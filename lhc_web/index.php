8ba401fe1008161b089c93fb2f7aadd4c71427be
Andrius Kučinskas <andriuskucinskas@yahoo.com>
2013-03-14 14:40:58 +0200

    Close button added

--- lhc_web/index.php
+++ lhc_web/index.php
@@ -1,8 +1,11 @@
 <?php
 
-@ini_set('error_reporting', 0);
-@ini_set('display_errors', 0);
-
+@ini_set('error_reporting', 1);
+@ini_set('display_errors', 1);
+@ini_set('session.gc_probability', 1);
+@ini_set('session.gc_divisor', 100);
+@ini_set('session.gc_maxlifetime', 200000);
+@ini_set('session.cookie_lifetime', 2000000);
 require_once "ezcomponents/Base/src/base.php"; // dependent on installation method, see below
 
 function __autoload( $className )
